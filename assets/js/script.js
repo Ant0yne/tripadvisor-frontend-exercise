de59d38833073a48a404167353d02f5e930ea0e4
Ant0yne <antoine.soliv@gmail.com>
2024-03-17 16:12:36 +0100

    form-working

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+const $ = document;
+
+$.addEventListener("DOMContentLoaded", () => {
+	const form = $.querySelector("form");
+	const contactButton = $.querySelector("header button");
+	const blackScreen = $.getElementById("black-screen");
+	const sectionForm = $.getElementById("contact-form");
+	const crossClose = $.querySelector(".form-container > div i");
+
+	contactButton.addEventListener("click", (e) => {
+		blackScreen.classList.toggle("hidden");
+		sectionForm.classList.toggle("hidden");
+	});
+
+	crossClose.addEventListener("click", (e) => {
+		blackScreen.classList.toggle("hidden");
+		sectionForm.classList.toggle("hidden");
+	});
+
+	form.addEventListener("submit", async (e) => {
+		e.preventDefault();
+
+		const data = {
+			firstname: $.getElementById("contact-form-fn").value,
+			lastname: $.getElementById("contact-form-ln").value,
+			email: $.getElementById("contact-form-e").value,
+			message: $.getElementById("contact-form-m").value,
+		};
+
+		try {
+			const response = await axios.post("http://localhost:3000/form", data);
+
+			console.log({
+				status: response.status,
+				message: response.data,
+			});
+		} catch (error) {
+			console.error({
+				status: error.response.status,
+				message: error.response.data.message,
+			});
+		}
+	});
+});
